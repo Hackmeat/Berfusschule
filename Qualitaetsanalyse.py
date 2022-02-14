f5ceb707c40d040d620c86f0ccf351eda873170d
Nico Beulich <beulichn@yahoo.de>
2022-02-14 12:12:03 +0100

    created getAvg function

--- Qualitaetsanalyse.py
+++ Qualitaetsanalyse.py
@@ -1,22 +1,26 @@
 #Initializing
 values = [4.92, 4.98, 4.97, 5.01, 5.02, 5.01, 5.03, 4.95, 4.96, 5.04]
 extrems = []
-i = 0
 sum = 0
 sumNoExtrem = 0
 base = 5
+i = 0
 
 #Adding up all numbers and catch & print extrem values
-while i < len(values):
-    sum += values[i]
-    if values[i] > base * 1.01 or values[i] < base * 0.99:
-        print("Out of range: " + str(values[i]))
-        extrems.append(values[i])
-    else:
-        sumNoExtrem += values[i]
-    i += 1
+def getAvrage():
+    global sum, sumNoExtrem, base, i
+    while i < len(values):
+        sum += values[i]
+        if values[i] > base * 1.01 or values[i] < base * 0.99:
+            print("Out of range: " + str(values[i]))
+            extrems.append(values[i])
+        else:
+            sumNoExtrem += values[i]
+        i += 1
      
 #Get the avarage value   
+getAvrage()
+
 sum = sum / len(values)
 sumNoExtrem = sumNoExtrem / (len(values) - len(extrems))
 

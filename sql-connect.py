78ff8fa76e7ad82de438317a4af1e0a0cd26353c
Nico Beulich <beulichn@yahoo.de>
2022-03-28 12:34:02 +0200

    added new sql statments

--- sql-connect.py
+++ sql-connect.py
@@ -12,10 +12,10 @@ con = mysql.connector.connect(
     password=Passwort)
 
 
-def all_articles():
+def run_sql(sql):
     cursor = con.cursor()
 
-    SQLBefehl = 'SELECT * FROM tbl_artikel'
+    SQLBefehl = sql
     cursor.execute(SQLBefehl)
     row = cursor.fetchone()
 
@@ -24,4 +24,32 @@ def all_articles():
         row = cursor.fetchone()
 
 
-all_articles()
+#1) all Aricles
+#run_sql('SELECT * FROM tbl_artikel')
+
+#2) all Names an Prices
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel')
+
+#3) typ feder
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel WHERE Warentyp = "Feder"')
+
+#4) price > 20
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel WHERE ArtikelPreis > 20')
+
+#5) Date = 04.1.2022
+#run_sql('SELECT Artikelbezeichnung FROM tbl_artikel WHERE Kaufdatum = "2022-01-04"')
+
+#6) Date between 1.1.2020 and 1.1.2022
+#run_sql('SELECT Artikelbezeichnung FROM tbl_artikel WHERE (Kaufdatum BETWEEN "2020-01-01" AND "2022-01-01")')
+
+#7) contains liqui
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel WHERE Artikelbezeichnung LIKE "%Liqui%"')
+
+#8) typ Schmierstoff
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel WHERE Warentyp = "Schmierstoff"')
+
+#9) typ Schmierstoff alphabetisch order
+#run_sql('SELECT Artikelbezeichnung, ArtikelPreis FROM tbl_artikel WHERE Warentyp = "Schmierstoff" ORDER BY Artikelbezeichnung ASC')
+
+#10) all WarenTyp
+#run_sql('SELECT WarenTyp FROM tbl_artikel GROUP BY WarenTyp')
